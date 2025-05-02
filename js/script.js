03753eb3ce4ef93f08792cc2a217adc53d0a0b49
Fragas <141639158+FragasOficial@users.noreply.github.com>
2025-05-01 23:39:04 -0300

    Atualizando
    -Refatoração do html;
    -modal css;
    -código js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,10 @@
-// Você pode adicionar aqui funcionalidades JavaScript, como:
-// - Validação de formulários
-// - Efeitos de rolagem suave
-// - Carregamento dinâmico de conteúdo
-// - Interações específicas para a área do servidor
 
-// Exemplo de rolagem suave ao clicar nos links da navegação
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
+  // Mostra o modal ao carregar a página
+  window.onload = function () {
+    document.getElementById("modal-construcao").style.display = "flex";
+  };
 
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-// Outras funcionalidades podem ser adicionadas conforme a necessidade
+  // Fecha o modal ao clicar no botão
+  document.getElementById("fechar-modal").onclick = function () {
+    document.getElementById("modal-construcao").style.display = "none";
+  };
